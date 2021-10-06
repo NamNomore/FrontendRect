9797c5e744434110d8a18b1ca2ec92d7d840acf2
DESKTOP-MNURE78\Dell <Nampv520@wru.vn>
2021-10-06 23:19:23 +0700

    add

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
 import Nav from './component/nav.js';
 import Carousel from './component/carousel.js';
 import Content from './component/content.js';
-import Footer from './component/content.js';
+import Footer from './component/footer.js';
+import DieuHuongURl from './router/DieuHuongURl.js';
 function App() {
   return (
-    <div className="App">
+    <Router>
+    <div>
       <Nav></Nav>
       <Carousel></Carousel>
-      <Content></Content>
+      <DieuHuongURl></DieuHuongURl>
       <Footer></Footer>
     </div>
+    </Router>
   );
 }
 
---
+++ my-app/src/router/DieuHuongURl.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Route } from "react-router-dom";
+import Content from './../component/content.js';
+class DieuHuongURl extends Component {
+    render() {
+        return (
+            <Route>
+            <div>
+                    <Route exact path="/" component={Content} />                   
+                    <Route exact path="/home" component={Content} />                   
+                    {/* <Route exact path="/tin" component={News} />                   
+                    <Route exact path="/tin-tuc/:slug.:id.html" component={NewsDetail} />                   
+                    <Route exact path="/lien-he" component={Contact} />   */}
+            </div>
+            </Route>
+        );
+    }
+}
+
+export default DieuHuongURl;
